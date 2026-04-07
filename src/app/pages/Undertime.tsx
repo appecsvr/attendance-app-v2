943cce322b39085c9ce0958cb30a043dbb30142c
Nathaniel Philip Cruz <npcruz@appelectriccorp.onmicrosoft.com>
2026-04-07 15:57:19 +0800

    Describe your changes

--- src/app/pages/Undertime.tsx
+++ src/app/pages/Undertime.tsx
@@ -210,7 +210,7 @@ export function Undertime() {
               <div>
                 <h3 className="text-lg font-bold text-slate-900">Add Undertime</h3>
                 <p className="text-sm text-slate-500">
-                  Matching name + date will exclude that late record.
+                  date will exclude that late record.
                 </p>
               </div>
             </div>
